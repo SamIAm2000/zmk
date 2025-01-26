6602595d311d45fadedd28823f1d21bbec0938af
SamIAm2000 <84295663+SamIAm2000@users.noreply.github.com>
2025-01-25 22:35:45 -0500

    try cursor fix

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -70,7 +70,7 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_BONGO_CAT)
     zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
-    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), NULL, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_CENTER, 0, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
