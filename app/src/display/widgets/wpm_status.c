69a5be1b7c8b0965104065d0f6bc379741671a96
SamIAm2000 <84295663+SamIAm2000@users.noreply.github.com>
2025-01-26 17:42:25 +0000

    commented out move wpm to foreground

--- app/src/display/widgets/wpm_status.c
+++ app/src/display/widgets/wpm_status.c
@@ -48,7 +48,7 @@ int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *p
     lv_obj_align(widget->obj, LV_ALIGN_RIGHT_MID, 0, 0);
 
     // Move the WPM widget to the topmost layer
-    lv_obj_move_foreground(widget->obj);
+    //lv_obj_move_foreground(widget->obj);
 
     sys_slist_append(&widgets, &widget->node);
 
